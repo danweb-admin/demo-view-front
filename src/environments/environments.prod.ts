6568a828e0ce3d8928e8ef7286d15d0ab9b67657
William Nascimento <william.nascimento2010@gmail.com>
2025-09-04 19:30:54 -0300

    view

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'https://backend.swr-locacoes-agenda.online',
+  URL_API: 'https://backend.swr-locacoes-agenda.online', 
   // URL_API: 'http://95.111.226.231:32112',
   // URL_API: 'http://52.0.179.113:5000',
   hmr: false,
